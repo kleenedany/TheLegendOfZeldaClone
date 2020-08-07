f0079c3e44a27bcc6e609464e84a30e7fdb814f1
Daniela Rotter <rotterda@hs-furtwangen.de>
2020-08-07 22:29:01 +0200

    change

--- src/Main.ts
+++ src/Main.ts
@@ -66,7 +66,7 @@ namespace LegendofZelda {
     }
 
     async function loadJSONFiles(): Promise<void> {
-        let response: Response = await fetch("TheLegendOfZeldaClone/json/gameInfo.json");
+        let response: Response = await fetch("https://github.com/kleenedany/TheLegendOfZeldaClone/tree/master/json/gameInfo.json");
         offer = await response.text();
         jsonData = JSON.parse(offer);
     }
